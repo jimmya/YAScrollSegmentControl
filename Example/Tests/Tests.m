5c9e53b2647180c3af123e10c1a6b9e33d9c78d5
jimmy <arts.jimmy@gmail.com>
2015-02-10 20:44:12 +0100

    Removed tests
    Removed tests

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  YAScrollSegmentControlTests.m
-//  YAScrollSegmentControlTests
-//
-//  Created by jimmy on 02/09/2015.
-//  Copyright (c) 2014 jimmy. All rights reserved.
-//
-
-${TEST_EXAMPLE}
